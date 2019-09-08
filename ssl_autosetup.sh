3af0c92bbf3551f27c6a070b20e70a315468af66
Kazunori Kimura <norytama.k2@gmail.com>
2019-09-08 20:16:13 +0900

    stop using "firefox" command, use xdg-open instead
    close #14

--- ssl_autosetup.sh
+++ ssl_autosetup.sh
@@ -235,7 +235,7 @@ function install_dev_tools() {
                     exit
                 ;;
             esac
-            firefox -url "https://qiita.com/mfujimori/items/9fd41bcd8d1ce9170301"
+            xdg-open https://qiita.com/mfujimori/items/9fd41bcd8d1ce9170301
             ;;
         * )
             echo "Didn't install these tools."
